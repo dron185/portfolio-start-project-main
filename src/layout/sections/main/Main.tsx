b2779eb3d3c19136da6659d4e52fc8b60633b570
Andrei Huryn <gurin.nord@gmail.com>
2024-01-11 14:16:04 +0300

    add styles to main block

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import photo from "../../../assets/images/photo.jpg";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Container } from "../../../components/Container";
+import { theme } from "../../../styles/Theme";
 
 export const Main = () => {
   return (
@@ -11,10 +12,14 @@ export const Main = () => {
         <FlexWrapper align={"center"} justify={"space-between"}>
           <div>
             <SmallText>Hi There</SmallText>
-            <Name>I am Andrey Huryn</Name>
+            <Name>
+              I am <span>Andrey Huryn</span>
+            </Name>
             <MainTitle>A Web Developer.</MainTitle>
           </div>
-          <Photo src={photo} alt="main photo" />
+          <PhotoWrapper>
+            <Photo src={photo} alt="main photo" />
+          </PhotoWrapper>
         </FlexWrapper>
       </Container>
     </StyledMain>
@@ -27,15 +32,58 @@ const StyledMain = styled.section`
   display: flex;
 `;
 
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    content: '';
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    z-index: -1;
+  }
+`;
+
 const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
 `;
 
-const MainTitle = styled.h1``;
+const MainTitle = styled.h1`
+  font-size: 27px;
+  font-weight: 400;
+`;
 
-const Name = styled.h2``;
+const Name = styled.h2`
+  margin: 10px 0;
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 50px;
+  font-weight: 700;
+  letter-spacing: 0.05em;
+
+  span {
+    position: relative;
+    z-index: 0;
+
+    &::before {
+      content: "";
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+    }
+  }
+`;
 
 const SmallText = styled.span`
   font-size: 14px;
