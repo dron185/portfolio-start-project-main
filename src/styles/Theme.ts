7304b5006bea21669eff6d22d266da245422b696
Andrei Huryn <gurin.nord@gmail.com>
2024-01-12 16:09:30 +0300

    add styles to contacts section

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -4,6 +4,10 @@ export const theme = {
     secondaryBg: "#252527",
     accent: "#7572D5",
     
-    font: "#ffffff"
+    font: "#ffffff",
+
+    borderColor: "#4a4a4a",
+    
+    placeholderColor: "#495057"
   }
 }
