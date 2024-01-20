b4610910c20810854ec535164847093f99b1dbdc
Andrei Huryn <gurin.nord@gmail.com>
2024-01-20 16:12:52 +0300

    feat: add media to main section

--- src/layout/header/mobileMenu/MobileMenu.tsx
+++ src/layout/header/mobileMenu/MobileMenu.tsx
@@ -9,11 +9,11 @@ import { theme } from "../../../styles/Theme";
 export const MobileMenu = (props: { menuItems: Array<string> }) => {
     return (
         <StyledMobileMenu>
-            <BurgerButton isOpen={true}>
+            <BurgerButton isOpen={false}>
                 <span></span>
             </BurgerButton>
 
-            <MobileMenuPopup isOpen={true}>
+            <MobileMenuPopup isOpen={false}>
                 <ul>
                     {props.menuItems.map((item: string, index: number) => {
                         return (
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,88 +4,117 @@ import photo from "../../../assets/images/photo.jpg";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Container } from "../../../components/Container";
 import { theme } from "../../../styles/Theme";
+import { font } from "../../../styles/Common";
 
 export const Main = () => {
-  return (
-    <StyledMain>
-      <Container>
-        <FlexWrapper align={"center"} justify={"space-between"}>
-          <div>
-            <SmallText>Hi There</SmallText>
-            <Name>
-              I am <span>Svetlana Dyablo</span>
-            </Name>
-            <MainTitle>A Web Developer.</MainTitle>
-          </div>
-          <PhotoWrapper>
-            <Photo src={photo} alt="main photo" />
-          </PhotoWrapper>
-        </FlexWrapper>
-      </Container>
-    </StyledMain>
-  );
+    return (
+        <StyledMain>
+            <Container>
+                <FlexWrapper
+                    align={"center"}
+                    justify={"space-around"}
+                    wrap={"wrap"}
+                >
+                    <div>
+                        <SmallText>Hi There</SmallText>
+                        <Name>
+                            I am <span>Svetlana Dyablo</span>
+                        </Name>
+                        <MainTitle>A Web Developer.</MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={photo} alt="main photo" />
+                    </PhotoWrapper>
+                </FlexWrapper>
+            </Container>
+        </StyledMain>
+    );
 };
 
 const StyledMain = styled.section`
-  min-height: 100vh;
-  background-color: #fff5e7;
-  display: flex;
+    min-height: 100vh;
+    background-color: #fff5e7;
+    display: flex;
 `;
 
 const PhotoWrapper = styled.div`
-  position: relative;
-  z-index: 0;
-
-  &::before {
-    content: '';
-    width: 360px;
-    height: 470px;
-    border: 5px solid ${theme.colors.accent};
-
-    position: absolute;
-    top: -24px;
-    left: 24px;
-    z-index: -1;
-  }
-`;
-
-const Photo = styled.img`
-  width: 350px;
-  height: 430px;
-  object-fit: cover;
-`;
-
-const MainTitle = styled.h1`
-  font-size: 27px;
-  font-weight: 400;
-`;
-
-const Name = styled.h2`
-  margin: 10px 0;
-  font-family: "Josefin Sans", sans-serif;
-  font-size: 50px;
-  font-weight: 700;
-  letter-spacing: 0.05em;
-
-  span {
     position: relative;
     z-index: 0;
 
     &::before {
-      content: "";
-      display: inline-block;
-      width: 100%;
-      height: 20px;
-      background-color: ${theme.colors.accent};
+        content: "";
+        width: 360px;
+        height: 470px;
+        border: 5px solid ${theme.colors.accent};
 
-      position: absolute;
-      bottom: 0;
-      z-index: -1;
+        position: absolute;
+        top: -24px;
+        left: 24px;
+        z-index: -1;
+
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+            top: -20px;
+            left: 17px;
+        }
     }
-  }
+
+    @media ${theme.media.mobile} {
+        margin-top: 65px;
+    }
+`;
+
+const Photo = styled.img`
+    margin-right: 20px;
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 380px;
+    }
+`;
+
+const MainTitle = styled.h1`
+    ${font({ Fmax: 27, Fmin: 20 })}
+`;
+
+const Name = styled.h2`
+    ${font({
+        family: "'Josefin Sans', sans-serif",
+        weight: 700,
+        Fmax: 50,
+        Fmin: 36,
+    })}
+    margin: 10px 0;
+    letter-spacing: 0.05em;
+
+    span {
+        position: relative;
+        z-index: 0;
+        white-space: nowrap;
+
+        &::before {
+            content: "";
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent};
+
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
+
+    @media ${theme.media.mobile} {
+        margin: 15px 0 22px;
+    }
 `;
 
 const SmallText = styled.span`
-  font-size: 14px;
-  font-weight: 400;
+    font-size: 14px;
+    font-weight: 400;
 `;
