28f1743e6d2a3e7c8f4177e7972c7e4dd56b2439
Andrei Huryn <gurin.nord@gmail.com>
2024-01-22 10:57:17 +0300

    add media to skills section

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,26 +1,34 @@
 import styled from "styled-components";
 import { theme } from "../styles/Theme";
+import { font } from "../styles/Common";
 
 export const SectionTitle = styled.h2`
-  font-family: "Josefin Sans", sans-serif;
-  font-size: 36px;
-  font-weight: 600;
-  letter-spacing: 5px;
-  text-align: center;
-  margin-bottom: 90px;
+    ${font({
+        family: "'Josefin Sans', sans-serif",
+        weight: 600,
+        Fmax: 36,
+        Fmin: 30,
+    })}
+    letter-spacing: 5px;
+    text-align: center;
+    margin-bottom: 90px;
 
-  position: relative;
+    position: relative;
 
-  &::before {
-    content: "";
-    display: inline-block;
-    width: 55px;
-    height: 1px;
-    background-color: ${theme.colors.accent};
+    &::before {
+        content: "";
+        display: inline-block;
+        width: 55px;
+        height: 1px;
+        background-color: ${theme.colors.accent};
 
-    position: absolute;
-    bottom: -30px;
-    left: 50%;
-    transform: translateX(-50%);
-  }
+        position: absolute;
+        bottom: -30px;
+        left: 50%;
+        transform: translateX(-50%);
+
+        @media ${theme.media.mobile} {
+          bottom: -24px;
+        }
+    }
 `;
