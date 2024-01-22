4eb624fa950489e5cebb34e8b793b177d83030b2
Andrei Huryn <gurin.nord@gmail.com>
2024-01-22 12:49:25 +0300

    add media to works section

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -16,7 +16,7 @@ export const Works = () => {
       <Container>
         <SectionTitle>My Works</SectionTitle>
         <TabMenu menuItems={worksItems} />
-        <FlexWrapper justify={"space-between"} align={"flex-start"}>
+        <FlexWrapper justify={"space-between"} align={"flex-start"} wrap={"wrap"}>
           <Work
             title={"Social Network"}
             text={
@@ -37,4 +37,8 @@ export const Works = () => {
   );
 };
 
-const StyledWorks = styled.section``;
+const StyledWorks = styled.section`
+  ${FlexWrapper} {
+    gap: 30px;
+  }
+`;
