d695007f49666eb0b376b6ff1aff0ed257e483df
Andrei Huryn <gurin.nord@gmail.com>
2024-01-23 15:43:50 +0300

    refactoring header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,33 +1,34 @@
 import React from "react";
-import styled from "styled-components";
-import { Logo } from "../../components/logo/Logo";
-import { Container } from "../../components/Container";
-import { FlexWrapper } from "../../components/FlexWrapper";
-import { HeaderMenu } from "./headerMenu/HeaderMenu";
-import { MobileMenu } from "./mobileMenu/MobileMenu";
+import {Logo} from "../../components/logo/Logo";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {DesktopMenu} from "./headerMeny/desktopMenu/DesktopMenu";
+import {MobileMenu} from "./headerMeny/mobileMenu/MobileMenu";
+import {S} from "./Header_Styles"
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"];
 
-export const Header = () => {
-  return (
-    <StyledHeader>
-      <Container>
-        <FlexWrapper justify={"space-between"} align={"center"}>
-          <Logo />
-          <HeaderMenu menuItems={items} />
-          <MobileMenu menuItems={items} />
-        </FlexWrapper>
-      </Container>
-    </StyledHeader>
-  );
-};
+export const Header: React.FC = () => {
+    const [width, setWidth] = React.useState(window.innerWidth);
+    const breakpoint = 768;
 
-const StyledHeader = styled.header`
-  background: rgba(31, 31, 32, 0.90);
-  padding: 20px 0;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 99999;
-`;
+    React.useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth)
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+
+    return (
+        <S.Header>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+
+                    {width < breakpoint ? <MobileMenu menuItems={items}/> : <DesktopMenu menuItems={items}/>}
+
+                </FlexWrapper>
+            </Container>
+        </S.Header>
+    );
+};
