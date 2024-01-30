7e237dd004bf057b3190ebe8592430f319904581
Andrei Huryn <gurin.nord@gmail.com>
2024-01-30 19:15:07 +0300

    add animation framer motion to works section

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,14 +1,15 @@
 import React, {useState} from "react";
-import { SectionTitle } from "../../../components/SectionTitle";
+import {SectionTitle} from "../../../components/SectionTitle";
 import {TabMenu, TabsStatusType} from "./tabMeny/TabMenu";
-import { FlexWrapper } from "../../../components/FlexWrapper";
-import { Work } from "./work/Work";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Work} from "./work/Work";
 import socialImg from "../../../assets/images/proj-1.png";
 import timerImg from "../../../assets/images/proj-2.png";
-import { Container } from "../../../components/Container";
-import { S } from "./Works_Styles";
+import {Container} from "../../../components/Container";
+import {S} from "./Works_Styles";
+import {AnimatePresence, motion} from "framer-motion"
 
-const tabsItems: Array<{status: TabsStatusType, title: string}> = [
+const tabsItems: Array<{ status: TabsStatusType, title: string }> = [
     {
         title: "All",
         status: "all"
@@ -32,14 +33,44 @@ const worksData = [
         title: "Social Network",
         src: socialImg,
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        type: "spa"
+        type: "spa",
+        id: 1
     },
     {
         title: "Timer",
         src: timerImg,
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim",
-        type: "react"
+        type: "react",
+        id: 2
     },
+    {
+        title: "Social Network",
+        src: socialImg,
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        type: "spa",
+        id: 3
+    },
+    {
+        title: "Timer",
+        src: timerImg,
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim",
+        type: "react",
+        id: 4
+    },
+    {
+        title: "Social Network",
+        src: socialImg,
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        type: "spa",
+        id: 5
+    },
+    {
+        title: "Timer",
+        src: timerImg,
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim",
+        type: "react",
+        id: 6
+    }
 ];
 
 export const Works: React.FC = () => {
@@ -47,13 +78,13 @@ export const Works: React.FC = () => {
     let filteredWorks = worksData
 
     if (currentFilterStatus === "landing") {
-        filteredWorks = worksData.filter( work => work.type === "landing")
+        filteredWorks = worksData.filter(work => work.type === "landing")
     }
     if (currentFilterStatus === "react") {
-        filteredWorks = worksData.filter( work => work.type === "react")
+        filteredWorks = worksData.filter(work => work.type === "react")
     }
     if (currentFilterStatus === "spa") {
-        filteredWorks = worksData.filter( work => work.type === "spa")
+        filteredWorks = worksData.filter(work => work.type === "spa")
     }
 
     function changeFilterStatus(value: TabsStatusType) {
@@ -71,13 +102,26 @@ export const Works: React.FC = () => {
                     align={"flex-start"}
                     wrap={"wrap"}
                 >
-                    {filteredWorks.map((w) => {
-                      return <Work
-                      title={w.title}
-                      text={w.text}
-                      src={w.src}
-                  />
-                    })}
+                    <AnimatePresence>
+                        {filteredWorks.map((w) => {
+                            return (
+                                <motion.div style={{width: "400px", flexGrow: 1, maxWidth: "540px"}}
+                                    layout
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    key={w.id}
+                                >
+                                    <Work
+                                        title={w.title}
+                                        text={w.text}
+                                        src={w.src}
+                                        key={w.id}
+                                    />
+                                </motion.div>
+                            )
+                        })}
+                    </AnimatePresence>
 
                 </FlexWrapper>
             </Container>
